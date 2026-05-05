e4d0b939aeee2092c095fcc8f13e23ff8a743f8e
Yaw Fosu <codehumps00233@gmail.com>
2026-05-05 14:15:32 +0000

    dashboard redesign

--- app/(app)/layout.jsx
+++ app/(app)/layout.jsx
@@ -3,16 +3,16 @@
 
 import BottomTabBar from "@/components/layout/BottomTabBar";
 import ProtectedRoute from "@/components/layout/ProtectedRoutes";
-import QueryProvider from "@/components/providers/QueryProvider";
-import { Toaster } from "@/components/ui/sonner";
 
 export default function AppLayout({ children }) {
   return (
     <ProtectedRoute>
       <div className="min-h-screen bg-gray-50 dark:bg-gray-950">
-        {/* Remove max-w-7xl and mx-auto to allow full width */}
-        <main className="pb-20 p-4">{children}</main>
-        <BottomTabBar />
+        <main className="p-4 md:p-6">{children}</main>
+        {/* Only show BottomTabBar on mobile, hide on desktop */}
+        <div className="lg:hidden">
+          <BottomTabBar />
+        </div>
       </div>
     </ProtectedRoute>
   );
